6a84e8bb007b105c1eeae9aabc75a34192fe2ca4
Yunus <128239091+Botirkhoja@users.noreply.github.com>
2023-05-18 16:37:16 +0300

    Add files via upload

---
+++ 1-js uy ishi/app.js
@@ -0,0 +1,15 @@
+alert("Assalomu aleykum");
+
+alert("Mening ismim Yunus");
+
+prompt("Bot emasligingizni isbotlang");
+
+prompt("No'meringizni Qoldiring");
+
+let lap = "macbook"
+
+let phone = "Iphone"
+
+console.log(lap);
+
+console.log(phone);
